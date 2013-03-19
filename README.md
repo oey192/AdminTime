d1111cd9788fa4f5096a12794cde992d52b23b5c
Orion <oeystanger@gmail.com>
2013-03-19 00:07:27 -0600

    Players no longer show up in /am list if they are vanished. The player that someone is helping shows up next to their name in /am list and players are able to specify a reason that they are helping someone. If they do, it also shows up in /am list

--- README.md
+++ README.md
@@ -3,19 +3,23 @@ AdminTime Readme
 
 AdminTime allows admins to switch in and out of an Admining state and allows them to resume where they left off when done admining
 
-Note: This plugin temporarily overrides permissions set by PermissionsEx when the appropriate config value is set to true. When this is set to false, this plugin only modifies permissions via the built-in Bukkit Pernussions API
+This plugin uses Essentials to enable God mode and Fly mode if the appropriate config values are set.
+
+Players will not be informed when vanished players (VanishNoPacket) enter Admin Mode, nor will vanished players show up in /admintime list.
+
+Note: If PermissionsEx is being used on your server, this plugin temporarily overrides permissions set by PermissionsEx when the appropriate config value is set to true. When this is set to false, this plugin only modifies permissions via the built-in Bukkit Pernussions API. This can cause certain permisisons used with this plugin to have no effect as some plugins (e.g. WorldEdit) talk directly to PermissionsEx to check what permissions a player has.
 
 <h2>Commands</h2>
 
 Aliases: at, am, adminmode
 
-/admintime [player]:<br/>Toggle admin mode. The name of the player being helped is required for entering admin mode. If that player is offline, their full name must be specified
+/admintime [player] [reason]:<br/>Toggle admin mode. The name of the player being helped is required for entering admin mode. If that player is offline, their full name must be specified. A reason may be specified if desired.
 
 /admintime reload:<br/>Reload the AdminTime permissions file
 
 /admintime version:<br/>Get the current version of AdminTime
 
-/admintime list [#]:<br/>List players in admin mode. Specify page numbers if necessary
+/admintime list [#]:<br/>List players in admin mode, who they're helping and the reason if specified. Use page numbers if necessary. (Inputting page numbers higher than the last page will result in the last page being shown)
 
 /admintime eject:<br/>Kick a player out of admin mode
 
--- src/com/andoutay/admintime/AdminTime.java
+++ src/com/andoutay/admintime/AdminTime.java
@@ -1,5 +1,6 @@
 package com.andoutay.admintime;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
@@ -13,12 +14,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kitteh.vanish.staticaccess.VanishNoPacket;
 
-public class AdminTime extends JavaPlugin {
+public class AdminTime extends JavaPlugin
+{
 	/*
-	 * TODO: Tie in with Essentials /back command when teleporting a player back
-	 * to their location when they're done admining
-	 * 
-	 * Keep track of who the player's helping and display this in /at list
+	 * TODO: 
 	 * 
 	 * Add mChat support (see below)
 	 * 
@@ -34,18 +33,24 @@ public class AdminTime extends JavaPlugin {
 	public static String chPref = ChatColor.LIGHT_PURPLE + logPref + ChatColor.RESET;
 	public static HashMap<Player, Boolean> inAdminMode;
 	public static HashMap<Player, Location> lastLocs;
+	public static HashMap<Player, String> helping;
+	public static HashMap<Player, String> reasons;
 	public static ATPermissionsFileHandler permHandler;
 	public static Server server;
 
-	public void onLoad() {
+	public void onLoad()
+	{
 		server = getServer();
 		new ATConfig(this);
 		permHandler = new ATPermissionsFileHandler(this);
 	}
 
-	public void onEnable() {
+	public void onEnable()
+	{
 		inAdminMode = new HashMap<Player, Boolean>();
 		lastLocs = new HashMap<Player, Location>();
+		helping = new HashMap<Player, String>();
+		reasons = new HashMap<Player, String>();
 
 		ATConfig.onEnable();
 		permHandler.onEnable();
@@ -54,11 +59,13 @@ public class AdminTime extends JavaPlugin {
 		log.info(logPref + "Enabled");
 	}
 
-	public void onDisable() {
+	public void onDisable()
+	{
 		log.info(logPref + "Disabled");
 	}
 
-	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args)
+	{
 		if (isReload(cmd.getName(), args))
 			return reloadConfig(s);
 		else if (isVersion(cmd.getName(), args))
@@ -75,64 +82,83 @@ public class AdminTime extends JavaPlugin {
 		return false;
 	}
 
-	private boolean isToggle(String cmdName, String[] args) {
-		return cmdName.equalsIgnoreCase("admintime") && (args.length == 0 || args.length == 1);
+	private boolean isToggle(String cmdName, String[] args)
+	{
+		return cmdName.equalsIgnoreCase("admintime") && (args.length >= 0);
 	}
 
-	private boolean isReload(String cmdName, String[] args) {
+	private boolean isReload(String cmdName, String[] args)
+	{
 		return cmdName.equalsIgnoreCase("admintime") && args.length == 1 && args[0].equalsIgnoreCase("reload");
 	}
 
-	private boolean isVersion(String cmdName, String[] args) {
+	private boolean isVersion(String cmdName, String[] args)
+	{
 		return cmdName.equalsIgnoreCase("admintime") && args.length == 1 && args[0].equalsIgnoreCase("version");
 	}
 
-	private boolean isEject(String cmdName, String[] args) {
+	private boolean isEject(String cmdName, String[] args)
+	{
 		return cmdName.equalsIgnoreCase("admintime") && args.length == 2 && args[0].equalsIgnoreCase("eject");
 	}
 
-	private boolean isList(String cmdName, String[] args) {
+	private boolean isList(String cmdName, String[] args)
+	{
 		return cmdName.equalsIgnoreCase("admintime") && (args.length == 1 || args.length == 2) && args[0].equalsIgnoreCase("list");
 	}
 
-	private boolean isHelp(String cmdName, String[] args) {
+	private boolean isHelp(String cmdName, String[] args)
+	{
 		return cmdName.equalsIgnoreCase("admintime") && args.length == 1 && args[0].equalsIgnoreCase("help");
 	}
 
-	private boolean toggle(CommandSender s, String[] args) {
+	private boolean toggle(CommandSender s, String[] args)
+	{
 		if (s instanceof ConsoleCommandSender)
 			return noConsoleAccess(s);
 		else if (!(s.hasPermission("admintime.toggle.self") || s.hasPermission("admintime.toggle.others")))
 			return noAccess(s);
 
 		Player p = (Player) s;
+		String rsn = "";
 		if (!inAdminMode.containsKey(p))
 			inAdminMode.put(p, false);
 
 		inAdminMode.put(p, !inAdminMode.get(p));
 
-		if (inAdminMode.get(p)) {
-			if (args.length == 0) {
+		if (inAdminMode.get(p))
+		{
+			if (args.length == 0)
+			{
 				inAdminMode.put(p, false);
 				return missingArgument(s);
-			} else if (getPlayerForName(args[0]) == null && !getServer().getOfflinePlayer(args[0]).hasPlayedBefore()) {
+			}
+			else if (getPlayerForName(args[0]) == null && !getServer().getOfflinePlayer(args[0]).hasPlayedBefore())
+			{
 				inAdminMode.put(p, false);
 				return playerNotFound(s);
 			}
+			
+			if (args.length > 1)
+				rsn = implode(Arrays.copyOfRange(args, 1, args.length));
 
 			String str = getPlayerName(args[0]);
 			if (!((p.hasPermission("admintime.toggle.self") && p.getName().equalsIgnoreCase(str)) || (p.hasPermission("admintime.toggle.others") && !p
-				.getName().equalsIgnoreCase(str)))) {
+				.getName().equalsIgnoreCase(str))))
+			{
 				inAdminMode.put(p, false);
 				return badPlayer(p);
 			}
-			if (str == "")
-				str = args[0];
+			if (str == "") str = args[0];
 			lastLocs.put(p, p.getLocation());
+			helping.put(p, str);
+			reasons.put(p, rsn);
 			permHandler.enterAdminMode(p, p.getWorld().getName());
 			tellAll(p, "entered", str);
 			p.sendMessage(chPref + ChatColor.RED + "You are now in Admin Mode!");
-		} else {
+		}
+		else
+		{
 			permHandler.exitAdminMode(p, p.getWorld().getName());
 			p.teleport(lastLocs.get(p));
 			lastLocs.remove(p);
@@ -143,7 +169,8 @@ public class AdminTime extends JavaPlugin {
 		return true;
 	}
 
-	private boolean reloadConfig(CommandSender s) {
+	private boolean reloadConfig(CommandSender s)
+	{
 		if (!(s instanceof ConsoleCommandSender || (s instanceof Player && ((Player) s).hasPermission("admintime.reload"))))
 			return noAccess(s);
 
@@ -155,7 +182,8 @@ public class AdminTime extends JavaPlugin {
 		return true;
 	}
 
-	private boolean showVersion(CommandSender s) {
+	private boolean showVersion(CommandSender s)
+	{
 		if (!(s instanceof ConsoleCommandSender || (s instanceof Player && ((Player) s).hasPermission("admintime.version"))))
 			return noAccess(s);
 
@@ -163,7 +191,8 @@ public class AdminTime extends JavaPlugin {
 		return true;
 	}
 
-	private boolean eject(CommandSender s, String[] args) {
+	private boolean eject(CommandSender s, String[] args)
+	{
 		if (!(s instanceof ConsoleCommandSender || (s instanceof Player && ((Player) s).hasPermission("admintime.eject"))))
 			return noAccess(s);
 
@@ -182,18 +211,21 @@ public class AdminTime extends JavaPlugin {
 		return true;
 	}
 
-	private boolean showList(CommandSender s, String[] args) {
+	private boolean showList(CommandSender s, String[] args)
+	{
 		if (!(s instanceof ConsoleCommandSender || (s instanceof Player && ((Player) s).hasPermission("admintime.list"))))
 			return noAccess(s);
 
-		if (inAdminMode.size() == 0) {
+		if (inAdminMode.size() == 0)
+		{
 			s.sendMessage(chPref + "No players are currently in Admin Mode");
 			return true;
 		}
 
 		int page = 0;
 
-		try {
+		try
+		{
 			page = ((args.length == 2) ? Integer.parseInt(args[1]) : 1);
 		} catch (NumberFormatException e) {
 			return invalidArgument(s);
@@ -204,11 +236,13 @@ public class AdminTime extends JavaPlugin {
 		return true;
 	}
 
-	public void showListToSender(CommandSender s, int page) {
+	public void showListToSender(CommandSender s, int page)
+	{
 		final int perPage = 9;
 		final int totPages = inAdminMode.size() / perPage + 1;
 		Object players[] = inAdminMode.keySet().toArray();
 		Object values[] = inAdminMode.values().toArray();
+		Player p = null;
 
 		if (page > totPages)
 			page = totPages;
@@ -219,62 +253,79 @@ public class AdminTime extends JavaPlugin {
 		int start = (page - 1) * perPage;
 		int end = (totPages > page * perPage) ? totPages : page * perPage;
 		boolean found = false;
-		for (int i = start; i < end && i < inAdminMode.size(); i++) {
-			if ((Boolean) values[i]) {
-				found = true;
-				s.sendMessage(((Player) players[i]).getDisplayName());
-			} else if (i >= end)
-				i--;
+		for (int i = start; i < end && i < inAdminMode.size(); i++)
+		{
+			if ((Boolean) values[i])
+			{
+				p = (Player) players[i];
+				if ((s instanceof ConsoleCommandSender) || (  !isVanished(p.getName()) || canSee((Player)s, p)))
+				{
+					found = true;
+					s.sendMessage(ChatColor.AQUA + p.getDisplayName() + ChatColor.RESET + " - " + (helping.get(p).equalsIgnoreCase(p.getName()) ? "themself" : helping.get(p)) + (reasons.get(p).length() > 0 ? " (" + reasons.get(p) + ")" : ""));
+				}
+				else
+					end++;
+			}
+			else if (i >= end)
+				end++;
 		}
 
 		if (!found)
 			s.sendMessage("No players are currently in Admin Mode");
 	}
 
-	private boolean showHelp(CommandSender s) {
+	private boolean showHelp(CommandSender s)
+	{
 		s.sendMessage(chPref + "Help:");
 		s.sendMessage("Aliases: at, am, adminmode");
-		s.sendMessage("/admintime [player]: Toggle admin mode. The name of the player being helped is required for entering admin mode. If that player is offline, their full name must be specified");
+		s.sendMessage("/admintime [player] [reason]: Toggle admin mode. The name of the player being helped is required for entering admin mode. If that player is offline, their full name must be specified. You may specify a reason that you are helping the player");
 		s.sendMessage("/admintime reload: Reload the AdminTime permissions file");
 		s.sendMessage("/admintime version: Get the current version of AdminTime");
-		s.sendMessage("/admintime list [#]: List players in admin mode. Specify page numbers if necessary");
+		s.sendMessage("/admintime list [#]: List players in admin mode, who they're helping and the reason, if any. Specify page numbers if necessary");
 		s.sendMessage("/admintime eject: Kick a player out of admin mode");
 		s.sendMessage("/admintime help: Show this help message");
 
 		return true;
 	}
 
-	private boolean noAccess(CommandSender s) {
+	private boolean noAccess(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "You do not have access to that command");
 		return true;
 	}
 
-	private boolean noConsoleAccess(CommandSender s) {
+	private boolean noConsoleAccess(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "You must be in-game to use that command");
 		return true;
 	}
 
-	private boolean playerNotFound(CommandSender s) {
+	private boolean playerNotFound(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "Player not found");
 		return true;
 	}
 
-	private boolean invalidArgument(CommandSender s) {
+	private boolean invalidArgument(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "Invalid argument");
 		return false;
 	}
 
-	private boolean missingArgument(CommandSender s) {
+	private boolean missingArgument(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "You must supply another argument to use that command");
 		return false;
 	}
 
-	private boolean badPlayer(CommandSender s) {
+	private boolean badPlayer(CommandSender s)
+	{
 		s.sendMessage(ChatColor.RED + "You may not help that player");
 		return true;
 	}
 
-	public Player getPlayerForName(String partial) {
+	public Player getPlayerForName(String partial)
+	{
 		Player player = null;
 		boolean found = false, foundMult = false;
 
@@ -282,8 +333,10 @@ public class AdminTime extends JavaPlugin {
 
 		if (player == null)
 			for (Player p : getServer().getOnlinePlayers())
-				if (p.getDisplayName().toLowerCase().contains(partial.toLowerCase())) {
-					if (found) {
+				if (p.getDisplayName().toLowerCase().contains(partial.toLowerCase()))
+				{
+					if (found)
+					{
 						foundMult = true;
 						break;
 					}
@@ -297,7 +350,8 @@ public class AdminTime extends JavaPlugin {
 		return player;
 	}
 
-	public String getPlayerName(String partial) {
+	public String getPlayerName(String partial)
+	{
 		Player temp = getPlayerForName(partial);
 		if (temp == null)
 			return getServer().getOfflinePlayer(partial).getName();
@@ -309,8 +363,10 @@ public class AdminTime extends JavaPlugin {
 		 */
 	}
 
-	private static boolean isVanished(String name) {
-		try {
+	private static boolean isVanished(String name)
+	{
+		try
+		{
 			if (server.getPluginManager().getPlugin("VanishNoPacket") != null)
 				return VanishNoPacket.isVanished(name);
 		} catch (Exception localException) {
@@ -318,8 +374,10 @@ public class AdminTime extends JavaPlugin {
 		return false;
 	}
 
-	private boolean canSee(Player looking, Player uncertain) {
-		try {
+	private boolean canSee(Player looking, Player uncertain)
+	{
+		try
+		{
 			if (server.getPluginManager().getPlugin("VanishNoPacket") != null)
 				return VanishNoPacket.canSee(looking, uncertain);
 		} catch (Exception localException) {
@@ -328,11 +386,24 @@ public class AdminTime extends JavaPlugin {
 	}
 
 	public void tellAll(Player player, String msg, String recipient)
-	  {
-		for (Player p : getServer().getOnlinePlayers()) {
+	{
+		for (Player p : getServer().getOnlinePlayers())
+		{
 	    	if (p.hasPermission("admintime.notify") && (!isVanished(p.getName()) || canSee(p, player)))
 	    			p.sendMessage(ChatColor.WHITE + player.getDisplayName() + " " + ChatColor.GRAY + msg + " Admin Mode" + (recipient.equalsIgnoreCase("") ? "!" : new StringBuilder(" to help ").append(recipient.equalsIgnoreCase(player.getDisplayName()) ? "themself" : recipient).append("!").toString()));
 	    }
 	    log.info(logPref + player.getName() + " " + msg + " Admin Mode" + (recipient.equalsIgnoreCase("") ? "!" : new StringBuilder(" to help ").append(recipient.equalsIgnoreCase(player.getDisplayName()) ? "themself" : recipient).append("!").toString()));
-	  }
+	}
+	
+	public String implode(String[] str)
+	{
+		String ans = "";
+		
+		for (String temp : str)
+			ans += temp + " ";
+		
+		ans = ans.substring(0, ans.length() - 1);
+		
+		return ans;
+	}
 }
